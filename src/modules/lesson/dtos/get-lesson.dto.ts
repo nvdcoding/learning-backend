8ad986e4b01cc2fabf6421ade5ee7b72f938ea8d
Duy Nguyen 5 <duy.nguyen5@sotatek.com>
2023-04-23 00:16:18 +0700

    daupte

--- src/modules/lesson/dtos/get-lesson.dto.ts
+++ src/modules/lesson/dtos/get-lesson.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GetLessonDto {
   @ApiProperty()
-  @IsNumber()
+  @IsString()
   @IsNotEmpty()
   courseId: number;
 }
