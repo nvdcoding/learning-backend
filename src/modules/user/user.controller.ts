c60b7852963f7a3527802f23833253a5d37e2cb0
Duy Nguyen 5 <duy.nguyen5@sotatek.com>
2023-05-04 01:37:03 +0700

    12

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -20,7 +20,7 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/')
+  @Get('/ipn')
   async webhookIPN(@Query() query) {
     return this.userService.IPNUrl(query);
   }
