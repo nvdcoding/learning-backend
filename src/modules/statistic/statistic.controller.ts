41fc6ea440a6f297da3e4561ce0e08b478fae8ec
Duy Nguyen 5 <duy.nguyen5@sotatek.com>
2023-06-07 00:05:33 +0700

    1

--- src/modules/statistic/statistic.controller.ts
+++ src/modules/statistic/statistic.controller.ts
@@ -3,6 +3,7 @@ import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { BasePaginationRequestDto } from 'src/shares/dtos/base-pagination.dto';
 import { Response } from 'src/shares/response/response.interface';
 import { AdminAuthGuard } from '../auth/guard/admin-auth.guard';
+import { AdminModAuthGuard } from '../auth/guard/admin-mod-auth-guard';
 import { StatisticService } from './statistic.service';
 
 @Controller('statistic')
@@ -11,7 +12,7 @@ import { StatisticService } from './statistic.service';
 export class StatisticController {
   constructor(private readonly statisticService: StatisticService) {}
   @Get('/transaction')
-  @UseGuards(AdminAuthGuard)
+  @UseGuards(AdminModAuthGuard)
   async getTransactions(
     @Query() options: BasePaginationRequestDto,
   ): Promise<Response> {
@@ -19,13 +20,13 @@ export class StatisticController {
   }
 
   @Get('/systems')
-  @UseGuards(AdminAuthGuard)
+  @UseGuards(AdminModAuthGuard)
   async getInfoSystem(): Promise<Response> {
     return this.statisticService.getInfoSystem();
   }
 
   @Get('/chart')
-  @UseGuards(AdminAuthGuard)
+  @UseGuards(AdminModAuthGuard)
   async getChart(): Promise<Response> {
     return this.statisticService.getChartCourse();
   }
